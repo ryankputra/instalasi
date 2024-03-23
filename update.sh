04c9f835020f968ddef2a3fa120e1532ef988cf8
ryankputra <48169572+ryankputra@users.noreply.github.com>
2024-03-23 12:04:26 +0700

    update.sh

--- update.sh
+++ update.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 # System Request : Debian 9+/Ubuntu 18.04+/20+
-# Develovers » Gemilangkinasih࿐
-# Email      » gemilangkinasih@gmail.com
-# telegram   » https://t.me/gemilangkinasih
-# whatsapp   » wa.me/+628984880039
+# Develovers » RyyStore࿐
+# tele grup  » https://t.me/ryyvpntesti
+# telegram   » https://t.me/RyyVpn26
+# whatsapp   » wa.me/+6287767287284
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-# Gemilangkinasih࿐
+# RyyStore࿐
 
 clear
 fun_bar() {
