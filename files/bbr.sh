0922d5d38842d64c745450c102f58cdb06913d45
ryankputra <48169572+ryankputra@users.noreply.github.com>
2024-03-24 09:07:32 +0700

    bbr.sh

--- files/bbr.sh
+++ files/bbr.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 # System Request : Debian 9+/Ubuntu 18.04+/20+
-# Develovers » Gemilangkinasih࿐
-# Email      » gemilangkinasih@gmail.com
-# telegram   » https://t.me/gemilangkinasih
-# whatsapp   » wa.me/+628984880039
+# Develovers » RyyStore࿐
+# telegrup   » https://t.me/ryyvpntesti
+# telegram   » https://t.me/RyyVpn26
+# whatsapp   » wa.me/+6287767287284
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-# Gemilangkinasih࿐
+# RyyStore࿐
 
 red='\e[1;31m'
 green='\e[0;32m'
